a9c95bbb2b0d3cbe9aebb8187bfab1805d092423
Alex Kovrigin <a.kovrigin0@gmail.com>
2019-07-19 19:42:43 +0300

    HOME PAGE WORKS OMG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cavoke-web';
+  loadAPI: Promise<any>;
+
+  constructor() {
+    this.loadAPI = new Promise((resolve) => {
+      this.loadScript();
+      resolve(true);
+    });
+  }
+
+  public loadScript() {
+    let isFound = false;
+    const scripts = document.getElementsByTagName('script');
+    for (let i = 0; i < scripts.length; ++i) {
+      if (scripts[i].getAttribute('src') != null && scripts[i].getAttribute('src').includes('loader')) {
+        isFound = true;
+      }
+    }
+
+    if (!isFound) {
+      const dynamicScripts = ['/assets/js/main.js'];
+
+      for (let i = 0; i < dynamicScripts .length; i++) {
+        const node = document.createElement('script');
+        node.src = dynamicScripts [i];
+        node.type = 'text/javascript';
+        node.async = false;
+        node.charset = 'utf-8';
+        document.getElementsByTagName('head')[0].appendChild(node);
+      }
+
+    }
+  }
 }
