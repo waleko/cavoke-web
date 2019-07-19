c303f453c3da20c00132340e94475d912bbb5428
Alex Kovrigin <a.kovrigin0@gmail.com>
2019-07-19 13:13:53 +0300

    add not working ftco animated stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+import { appRoutes } from './routerConfig';
+import {HomeComponent} from './components/home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/routerConfig.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import {NotfoundComponent} from '../../../../../../WebstormProjects/untitled3/src/app/components/notfound/notfound.component';
+import {HomeComponent} from './components/home/home.component';
+
+export const appRoutes: Routes = [
+  { path: '',
+  component: HomeComponent}
+];
